77245de4e81496266d4157454d8b9a8b7e88d7b7
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-09-15 13:33:14 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/bento_grid/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bento_grid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:bento_grid/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: MainScreen(),
+    ),
+  );
+}
